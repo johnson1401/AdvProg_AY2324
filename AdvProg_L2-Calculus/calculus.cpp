4264a2d7d003a495ed4249d39ee778a71863ece8
Hoàng Sơn <sorrylight1@gmail.com>
2024-03-21 23:18:44 +0700

    first

---
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::stod;
+
+
+double mySin(double x);
+double myCos(double x);
+double mySqrt(double x);
+
+/***
+    Args:
+        x (double): a number
+    Returns:
+        double: cosine of x
+***/
+double myCos(double x) 
+{
+    return cos(x);
+}
+
+/***
+    Args:
+        x (double): a number
+    Returns:
+        double: sine of x
+***/
+double mySin(double x)
+{
+    return sin(x);
+}
+
+
+/***
+    Args:
+        x (double): a number
+    Returns:
+        double: square root of x
+***/
+double mySqrt(double x) {
+    if (x < 0) {
+        cout << "Invalid argument" << endl;
+        exit(1);
+    }
+
+    
+    return sqrt(x);
+}
